ad3bf07bca6f020795b998aa08788aa4f0012936
henry <wxiao1214@gmail.com>
2021-01-12 23:53:34 +0800

    firebase api ke

--- src/firebase.js
+++ src/firebase.js
@@ -1,28 +1,29 @@
-import * as firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firestore'
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 // firebase init
-const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: ''
-}
-firebase.initializeApp(firebaseConfig)
+var firebaseConfig = {
+  apiKey:"",
+  authDomain: "vuefirebase-7bf07.firebaseapp.com",
+  databaseURL: "https://vuefirebase-7bf07.firebaseio.com",
+  projectId: "vuefirebase-7bf07",
+  storageBucket: "vuefirebase-7bf07.appspot.com",
+  messagingSenderId: "65211879809",
+  appId: "1:65211879909:web:3ae38ef1cdcb2e01fe5f0c",
+  measurementId: "G-8GSGZQ44ST",
+};
+firebase.initializeApp(firebaseConfig);
 
 // utils
-const db = firebase.firestore()
-const auth = firebase.auth()
+const db = firebase.firestore();
+const auth = firebase.auth();
 
 // collection references
-const usersCollection = db.collection('users')
-const postsCollection = db.collection('posts')
-const commentsCollection = db.collection('comments')
-const likesCollection = db.collection('likes')
+const usersCollection = db.collection("users");
+const postsCollection = db.collection("posts");
+const commentsCollection = db.collection("comments");
+const likesCollection = db.collection("likes");
 
 // export utils/refs
 export {
@@ -31,5 +32,5 @@ export {
   usersCollection,
   postsCollection,
   commentsCollection,
-  likesCollection
-}
+  likesCollection,
+};
